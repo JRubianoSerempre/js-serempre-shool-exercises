751a3ff1bae7eb1c6400cad9a69d9896da3b931d
JRubianoSerempre <95040997+JRubianoSerempre@users.noreply.github.com>
2021-11-30 07:27:41 -0500

    Add files via upload

--- validations.js
+++ validations.js
@@ -1,23 +1,55 @@
-//Get values of the form and set variables
+//Declaration variables(get values)
 let names = document.getElementById('name');
-let cellphone = document.getElementById('phone-number');
-let email = document.getElementById("email");
-let country = document.getElementById("country");
+let cel = document.getElementById('phone-number');
+let email = document.getElementById('email');
+let country = document.getElementById('country');
 
-document.querySelector('form').onsubmit = function() {validate()};
+
+//Declaration variables for error(set values)
+let errorname = document.getElementById('errorname');
+let errcel = document.getElementById('errorphone');
+let errmail = document.getElementById('errmail');
+let errcountry = document.getElementById('errcountry');
+
 
 function validate(){
-
     const space=/^\s+$/;//RegEx for space
-    const emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
 
+// Validation of Contact Name
     if(names.value==null || names.value ==''){
-        const errorname = document.createElement('span');
-        errorname.innerHTML ='Campo requerido';
-        names.appendChild(errorname);
+        errorname.innerHTML='Campo requerido';
     }
-    
+//validation cellphone
+    if(cel==null || cel.value ==''|| space.test(cel.value)){
+        errcel.innerHTML='Campo requerido';
+    }else if(isNaN(cel.value)){
+            errcel.innerHTML='No es un número';
+    }else if(cel.length >10){
+    errcel.innerHTML='Debe ser un número menor a 10 digitos';
 }
 
+//Validation email
+//Regular Expression
+const emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
+/****************************************************************************/
+    if(email.value == null || email.value ==''){
+        errmail.innerHTML = 'Campo requerido';
+    }else if(!emailRegex.test(email.value)){  //format validation with regular expressions
+        errmail.innerHTML = 'Correo no válido';
+    }
+//Validation country
+    if(country.value=='default-option'){
+        errcountry.innerHTML='Campo requerido';
+    }else if(country.value=='CO'){
+        alert('Bienvenido '+names.value+' de Colombia');
+    }else if(country.value=='ESP'){
+        alert('Bienvenido '+names.value+'de España, TIO');
+    }else if(country.value=='ARG'){
+        alert('Bienvenido '+names.value+' de Argentina CHE!');
+    }else if(country.value=='BRA'){
+        alert('Bem vindo '+names.value+' do brasil')
+    }
 
 
+    return false;
+}
